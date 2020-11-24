9eada50cacd49f56756079ec96f22c3adc4f0488
Lucas Pickering <lucas@lucaspickering.me>
2020-11-23 19:05:38 -0500

    input stuff

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -3,8 +3,8 @@ mod tank;
 pub use tank::*;
 
 use crate::motors::Motor;
-use anyhow::anyhow;
 use gilrs::{Gamepad, GamepadId, Gilrs};
+use log::{debug, error, info, trace};
 
 pub trait InputMapping {
     fn motor_value(&self, gamepad: Gamepad<'_>, motor: Motor) -> f32;
@@ -13,30 +13,66 @@ pub trait InputMapping {
 #[derive(Debug)]
 pub struct InputHandler<T: InputMapping> {
     gil: Gilrs,
-    gamepad_id: GamepadId,
     mapping: T,
+    // Active gamepad in use. None if there is none connected.
+    gamepad_id: Option<GamepadId>,
 }
 
 impl<T: InputMapping> InputHandler<T> {
-    pub fn new(mapping: T) -> anyhow::Result<Self> {
+    pub fn new(mapping: T) -> Self {
         let gil = Gilrs::new().unwrap();
-        // Grab the first controller
-        let (gamepad_id, _) = gil
-            .gamepads()
-            .next()
-            .ok_or_else(|| anyhow!("Could not find any gamepads"))?;
-        Ok(Self {
+        let mut rv = Self {
             gil,
-            gamepad_id,
+            gamepad_id: None,
             mapping,
-        })
+        };
+
+        // Try to set up the gamepad. If none is present, just log an error and
+        // move on.
+        rv.init_gamepad();
+        if rv.gamepad_id.is_none() {
+            error!("No gamepad found, initializing without one")
+        }
+
+        rv
     }
 
-    fn gamepad(&self) -> Gamepad<'_> {
-        self.gil.gamepad(self.gamepad_id)
+    /// Initialize the input gamepad for this handler. If the gamepad is already
+    /// initialized, this does nothing. This can safely be called on every
+    /// loop, to enable hot-plugging.
+    pub fn init_gamepad(&mut self) {
+        // If we have no input set up, attempt to connect a new one
+        if self.gamepad_id.is_none() {
+            // Grab the first gamepad
+            match self.gil.gamepads().next() {
+                None => {
+                    trace!("No gamepad connected");
+                }
+                Some((gamepad_id, gamepad)) => {
+                    info!(
+                        "Using gamepad {} (id={})",
+                        gamepad.name(),
+                        gamepad_id
+                    );
+                    debug!(
+                        "Gamepad mapping source: {:?}",
+                        gamepad.mapping_source()
+                    );
+                    self.gamepad_id = Some(gamepad_id);
+                }
+            }
+        }
     }
 
-    pub fn motor_value(&self, motor: Motor) -> f32 {
-        self.mapping.motor_value(self.gamepad(), motor)
+    fn gamepad(&self) -> Option<Gamepad<'_>> {
+        self.gamepad_id.map(|id| self.gil.gamepad(id))
+    }
+
+    /// Get the value for a specific motor. The corresponding input value will
+    /// be looked up using the input mapping. Returns `None` if we have no
+    /// gamepad connected.
+    pub fn motor_value(&self, motor: Motor) -> Option<f32> {
+        self.gamepad()
+            .map(|gamepad| self.mapping.motor_value(gamepad, motor))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,26 @@ use log::{error, info};
 
 #[derive(Debug)]
 struct Robot {
-    input_handler: Option<InputHandler<TankMapping>>,
+    input_handler: InputHandler<TankMapping>,
     motors: Motors,
 }
 
 impl Robot {
     pub fn new() -> Self {
-        let input_handler = match InputHandler::new(TankMapping) {
-            Ok(handler) => Some(handler),
-            Err(err) => {
-                error!("Error initializing input handler: {}", err);
-                None
-            }
-        };
         Self {
-            input_handler,
+            input_handler: InputHandler::new(TankMapping),
             motors: Motors::new(),
         }
     }
 
     /// A single iteration of the main loop
     fn robot_loop(&mut self) {
+        // Try to connect to a gamepad. If we already have one connected, this
+        // won't do anything. This allows hot-plugging
+        self.input_handler.init_gamepad();
+
         for &motor in Motor::ALL_MOTORS {
-            let speed = self
-                .input_handler
-                .as_ref()
-                .map(|handler| handler.motor_value(motor))
-                .unwrap_or(0.0);
+            let speed = self.input_handler.motor_value(motor).unwrap_or(0.0);
             if let Err(err) = self.motors.set_speed(motor, speed) {
                 error!("{}", err);
             }
@@ -53,8 +46,10 @@ impl Robot {
 }
 
 fn main() {
+    // Initialize logger with default log level of `info`
     env_logger::Builder::from_env(Env::default().default_filter_or("info"))
         .init();
+
     info!("Initializing robot...");
     let mut robot = Robot::new();
     info!("Finished initialization");
